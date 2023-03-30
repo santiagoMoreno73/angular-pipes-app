a9b6c33aaa9b25da2ec16714fb437239b11cde7f
santiagoMoreno73 <santimor2009@hotmail.com>
2023-03-29 23:29:05 -0500

    add order component

--- src/app/ventas/pages/order/order.component.ts
+++ src/app/ventas/pages/order/order.component.ts
@@ -1,7 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Color, Hero } from '../../interfaces/ventas.interface';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
 })
-export class OrderComponent {}
+export class OrderComponent implements OnInit {
+  isUpper: boolean = false; 
+  orderBy: string = '';
+  heroes: Hero[] = [
+    {
+      name: 'Spiderman',
+      fly: false,
+      color: Color.red
+    },
+    {
+      name: 'Superman',
+      fly: true,
+      color: Color.blue
+    },
+    {
+      name: 'Batman',
+      fly: false,
+      color: Color.black
+    },
+    {
+      name: 'Robin',
+      fly: false,
+      color: Color.green
+    }
+  ];
+
+  constructor(){}
+
+  ngOnInit(){}
+
+  changeStatusText(){
+    this.isUpper = !this.isUpper
+  }
+
+  changeOrder(value: string){
+    this.orderBy = value;
+  }
+}
